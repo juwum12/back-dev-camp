ddc9abca0db881c4a80649ce8aaffa702eb212ea
juwum12 <juwum12@naver.com>
2024-05-21 01:29:03 +0900

    init commit

---
+++ src/main/java/com/sparta/backdevcamp/BackDevCampApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.backdevcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackDevCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackDevCampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/backdevcamp/BackDevCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.backdevcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackDevCampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
