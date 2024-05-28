2521ebbb73908e8060d7821f0a47be405436194c
juwum12 <juwum12@naver.com>
2024-05-29 01:43:03 +0900

    FEAT : LoggingInterceptor 추가

---
+++ src/main/java/com/sparta/backdevcamp/common/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.sparta.backdevcamp.common.config;
+
+import com.sparta.backdevcamp.common.logging.LoggingInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+    private final LoggingInterceptor loggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor)
+                .order(1)
+                .addPathPatterns("/**");
+    }
+}
